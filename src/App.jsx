27ce4c3d75ad6abc6ded8b1055fa9a47f75da274
Sagar Mitra <mitrasagar10@gmail.com>
2025-11-01 16:10:35 +0530

    style: add padding to App container for improved layout

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { Outlet } from "react-router-dom";
 const App = () => {
   return (
     <div>
-      <div className="w-full max-w-7xl mx-auto">
+      <div className="w-full max-w-7xl mx-auto px-5">
         <Navbar />
         <Outlet />
       </div>
