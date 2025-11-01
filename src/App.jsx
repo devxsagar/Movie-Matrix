bab5cbb4e4fc3eab15392109e28df07717015b32
Sagar Mitra <mitrasagar10@gmail.com>
2025-11-01 14:13:51 +0530

    feat: implement routing with react-router-dom and create Home, Details, and Navbar components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import { Button } from "@chakra-ui/react";
 import React from "react";
+import { Navbar } from "./components/Navbar";
+import { Outlet } from "react-router-dom";
 
 const App = () => {
   return (
-    <div >
-      <h1>Hello World</h1>
+    <div>
+      <Navbar />
+      <Outlet />
     </div>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
-import { Provider } from "./components/ui/provider";
+import {  RouterProvider } from "react-router-dom";
+import router from "./router";
 
-// import theme from "./theme";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Provider>
-      <App />
-    </Provider>
+      <RouterProvider router={router} />
   </StrictMode>
 );
