763a7745a09cf119e13318578b65df2404fed23f
Sagar Mitra <mitrasagar10@gmail.com>
2025-11-01 23:17:51 +0530

    fix: update parameter name in useSectionFetch hook

--- src/hooks/useSectionFetch.js
+++ src/hooks/useSectionFetch.js
@@ -1,12 +1,12 @@
 import { useEffect, useState } from "react";
 import { API_OPTIONS } from "../utils/constant"
 
-const useSectionFetch = ({ category, type }) => {
+const useSectionFetch = ({ category, mediaType }) => {
     const [data, setData] = useState(null);
 
   const getData = async () => {
     const response = await fetch(
-      `https://api.themoviedb.org/3/${category}/${type}/day?language=en-US`,
+      `https://api.themoviedb.org/3/${category}/${mediaType}/day?language=en-US`,
       API_OPTIONS
     );
     const json = await response.json();
