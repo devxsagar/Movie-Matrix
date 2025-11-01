d8f483bc584d4a320d7c882556288e87d9c974ae
Sagar Mitra <mitrasagar10@gmail.com>
2025-11-02 00:51:22 +0530

    feat: add sections in TVShows.jsx

--- src/pages/TVShows.jsx
+++ src/pages/TVShows.jsx
@@ -1,12 +1,18 @@
 import Banner from "@/components/Banner";
 import useSectionFetch from "@/hooks/useSectionFetch";
-import React, { use } from "react";
+import MediaSection from "@/sections/MediaSection";
+import React from "react";
 
 const TVShows = () => {
-    const bannerData = useSectionFetch({ category: "on_the_air", mediaType: "tv" });
-  return <div>
-    <Banner data={bannerData} />
-  </div>;
+  const bannerData = useSectionFetch({ category: "on_the_air", mediaType: "tv" });
+  return (
+    <div>
+      <Banner data={bannerData} />
+      <MediaSection title="airing today" category="airing_today" mediaType="tv" />
+      <MediaSection title="popular" category="popular" mediaType="tv" />
+      <MediaSection title="top rated" category="top_rated" mediaType="tv" />
+    </div>
+  );
 };
 
 export default TVShows;
